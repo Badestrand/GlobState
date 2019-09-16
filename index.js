f962aa59d6d1698d040b4aee05793ec076ec3605
Michel Helms <michelhelms@gmail.com>
2019-09-16 22:04:08 +0700

    Simplifications

--- index.js
+++ index.js
@@ -1,42 +1,11 @@
-/*
-
-Anforderungen:
- - Keinen inkonsistenten State
- 	=> Alles auf einmal setzen mit einer Art setState
- - Einfaches subscriben auch zu mehreren global states
-    => GlobState.subscribe(this, ['user', 'theme', 'counter']) ?
-
-Fragen/Entscheidungen:
- - Wie aus Component auf globalen State zugreifen? 
-   a) this.globals.user.username?
-   b) user.username? -> Yes, easiest!
-   c) this.state.user.username?
-
-Beispiele:
- - Counter
- - User management (login/logout)
- - Side-wide option theme (bright/dark)
-
-
-Noch Probleme:
- - 
-*/
-
 'use strict'
 
 const React = require('react')
 
 
 
-const globalsByName = {}
-
-
-
 class GlobState {
-	constructor(globalName) {
-		if (globalName !== undefined) {
-			globalsByName[globalName] = this
-		}
+	constructor() {
 		this._subscribers = []
 	}
 
@@ -52,51 +21,31 @@ class GlobState {
 	}
 
 	updateComponents() {
-		console.warn('GlobState: updateComponents is deprecated, please use updateListeners instead. It was renamed because you can now subscribe with simple functions as well: user.subscribe(() => console.log("Something about the user changed!")).')
-		this.updateListeners()
-	}
-
-	updateListeners() {
 		for (const subscriber of this._subscribers) {
-			if (subscriber.forceUpdate) {
-				subscriber.forceUpdate()
-			} else if (typeof subscriber === 'function') {
-				subscriber()
-			}
+			subscriber.forceUpdate()
 		}
 	}
 
-	static subscribe(component, globalNames) {
-		// Maybe the user accidentily passed a single global as a string instead of an array
-		if (typeof globalNames === 'string') {
-			globalNames = [globalNames]
-		}
-		for (const globalName of globalNames) {
-			const global = globalsByName[globalName]
-			if (!global) {
-				throw new Error('GlobState.subscribe: Could not find global \''+globalName+'\'')
-			}
-			global.subscribe(component)
-		}
-	}
 
-	static unsubscribe(component) {
-		// Just call unsubscribe for this component on all globals
-		for (const global of Object.values(globalsByName)) {
-			global.unsubscribe(component)
-		}
-	}
-
-	static connect(BaseComponent, globalNames) {
+	static connect(BaseComponent, ...globals) {
 		return class extends React.Component {
 			componentWillMount() {
-				GlobState.subscribe(this, globalNames)
+				// Just call subscribe for this component on all globals
+				for (const g of globals) {
+					g.subscribe(this)
+				}
 			}
 			componentWillUnmount() {
-				GlobState.unsubscribe(this)
+				// Just call unsubscribe for this component on all globals
+				for (const g of globals) {
+					g.unsubscribe(this)
+				}
 			}
 			render() {
-				return <BaseComponent {...this.props}/>
+				return React.createElement(
+					BaseComponent,
+					this.props
+				)
 			}
 		}
 	}
